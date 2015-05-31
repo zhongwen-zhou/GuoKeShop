ecb0c6be86b01f6a5478811154abe746963d176d
zhongwen <zhongwenzhou.wolf@gmail.com>
2015-05-31 21:42:45 +0800

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
 jiu = Category.create(name: '酒', level: 1)
 ['白酒','洋酒','啤酒','其他'].each do |name|
 	Category.create(name: name, level: 2, parent: jiu)
 end
 yl = Category.create(name: '饮料', level: 1)
-['碳酸饮料','果汁','矿泉水、纯净水','奶饮品','其他'].each do |name|
+['碳酸饮料','果汁','矿泉水','奶饮品','其他'].each do |name|
 	Category.create(name: name, level: 2, parent: yl)
 end
 ls = Category.create(name: '零食', level: 1)
-['饼干类','面包','薯片','水果干活'].each do |name|
+['饼干类','面包、派','薯片','水果干活','冰淇淋','肉类','果类','糖类','方便食品'].each do |name|
 	Category.create(name: name, level: 2, parent: ls)
+end
+
+qita = Category.create(name: '其他', level: 1)
+['生活用品'].each do |name|
+	Category.create(name: name, level: 2, parent: qita)
 end
